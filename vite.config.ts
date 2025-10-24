4b104e7fa02e64e3dfc5accf70d16b5811af6f40
AlexandreLeCam <a.lecam@myanapro.com>
2025-10-24 20:25:17 +0200

    deploy v1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://alex-lc-35.github.io/frama-vue/',
+  base: '/frama-vue/',
   plugins: [
     vue(),
     vueDevTools()
